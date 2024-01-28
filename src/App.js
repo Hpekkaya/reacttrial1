e06d8e1041d0f59071152c1710eb1c3c04ce6391
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-28 21:30:59 +0300

    First componenets created

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import React,{useState} from 'react';
-import './App.css';
-import Content from './00-Components/Content';
-
-const tema={
-  dark :{
-    color :"white",
-    backgroundColor : "black"
-  },
-  light :{
-    color :"black",
-    backgroundColor : "white"
-  }
-}
-export const TematicData = React.createContext(); //This should assigned here (outside of function)
-
 function App() {
-
-  const [deger,setDeger]=useState("Kerem Nadir Pekkaya")
-  function change(){
-    if(tema.dark===deger) {setDeger(tema.light); console.log(deger)}
-    else {setDeger(tema.dark); console.log(deger)}
-  }
-  
   return (
-    <div className="App">      
-       <button onClick={change}>
-        {tema.dark ===deger ? "Light" : "Dark"}
-        </button> 
+    <div>
+      <header>
+        <h2>React Essentials</h2>
 
-      <TematicData.Provider value={deger}>
-          <Content/>  
-      </TematicData.Provider>
+        <p>Fundemental React Concept</p>
+      </header>
 
-
+      <main>
+        <h2>Time to start</h2>
+      </main>
     </div>
   );
 }
-
-
-
 
 export default App;
