23a685c5269dc9c00c867489050b3dff9598c649
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-30 10:54:17 +0300

    Storing componenets in a project structure

---
+++ src/03 Section3/Lesson46.js
@@ -0,0 +1,48 @@
+import React from "react";
+import reactImage from "../assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data44";
+
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+const Lesson46 = () => {
+    return (
+        <div>
+          <Header />
+          <main>
+            <section id="core-concepts">
+              <h2>Core Concepts</h2>
+              <ul>
+                <CoreConcept {...CORE_CONCEPTS[0]}/>
+                <CoreConcept {...CORE_CONCEPTS[1]}/>
+                <CoreConcept {...CORE_CONCEPTS[2]}/>
+                <CoreConcept {...CORE_CONCEPTS[3]}/>
+                 
+              </ul>
+            </section>
+          </main>
+        </div>
+      );
+    };
+
+export default Lesson46
