bc06a72f3f240f01322b70d34ebc08bea0ad1680
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-30 09:16:28 +0300

    Alternative Props Syntaxes applied

--- src/03 Section3/Lesson44.js
+++ src/03 Section3/Lesson44.js
@@ -1,40 +1,49 @@
-import React from 'react'
+import React from "react";
 import reactImage from "../assets/react-core-concepts.png";
-import {CORE_CONCEPTS} from './data44'
+import { CORE_CONCEPTS } from "./data44";
 
+// function CoreConcept({image, title, description}) {
 function CoreConcept(props) {
-    return (
-      <li>
-        <img src={props.image} alt={props.title} />
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-      </li>
-    );
-  }
-  
-  function Header() {
-    return (
-      <header>
-        <img src={reactImage} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are going to build!
-        </p>
-      </header>
-    );
-    }
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
 
 const Lesson44 = () => {
   return (
     <div>
-        <Header/>
-        <CoreConcept
-            title={CORE_CONCEPTS[0].title} 
-            description="The core UI building block."
-            img={componentsImg}
-        />
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+             
+          </ul>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Lesson44
+export default Lesson44;
